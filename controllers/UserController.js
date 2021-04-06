1f5610c4e583187ae7607d9808f716d7bef0bf6a
ihsnmuh <muhammadihsan076@gmail.com>
2021-04-06 23:45:49 +0700

    Menambahkan Google Login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const { User } = require("../models");
 const { comparePassword } = require("../helpers/bcrypt");
 const { signToken } = require("../helpers/jwt");
+const { OAuth2Client } = require("google-auth-library");
 
 class UserController {
   static register(req, res, next) {
@@ -52,6 +53,62 @@ class UserController {
           // res.status(400).json({ message: "Invalid Email / Password" });
           throw { name: "InvalidUserorPassword" };
         }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
+  static googleLogin(req, res, next) {
+    const id_token = req.body.id_token;
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    let email = null;
+    let tempUsername = null;
+
+    client
+      .verifyIdToken({
+        idToken: id_token,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      })
+      .then((tiket) => {
+        const payload = tiket.getPayload();
+        console.log(payload);
+        email = payload.email;
+        tempUsername = email.split("@");
+
+        return User.findOne({
+          where: {
+            email: payload.email,
+          },
+        });
+      })
+      .then((DataUser) => {
+        if (DataUser) {
+          //kalau sudah terdaftar
+          return DataUser;
+        } else {
+          //kalau belum terdaftar
+          return User.create({
+            email: email,
+            username: tempUsername[0],
+            password: "passwordGoogle",
+          });
+        }
+      })
+      .then((DataUser) => {
+        console.log(User, "<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        const token = generateToken({
+          id: DataUser.id,
+          username: DataUser.username,
+          email: DataUser.email,
+        });
+        console.log(token, "<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        res.status(200).json({
+          id: DataUser.id,
+          username: DataUser.username,
+          email: DataUser.email,
+          access_token: token,
+        });
       })
       .catch((err) => {
         next(err);
